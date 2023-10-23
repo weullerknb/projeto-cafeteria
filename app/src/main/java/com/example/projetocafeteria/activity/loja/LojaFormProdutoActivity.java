11b894b9853581b7506d9eb26127f7581a1cb1a4
Weuller Oliveira <weullerwww@gmail.com>
2023-10-23 15:51:04 -0300

    Criado tela para add produto

---
+++ app/src/main/java/com/example/projetocafeteria/activity/loja/LojaFormProdutoActivity.java
@@ -0,0 +1,16 @@
+package com.example.projetocafeteria.activity.loja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projetocafeteria.R;
+
+public class LojaFormProdutoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loja_form_produto);
+    }
+}
