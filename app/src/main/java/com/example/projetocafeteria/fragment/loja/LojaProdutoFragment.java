9b09a52d276b599593175819fdad359775dee52b
Weuller Oliveira <weullerwww@gmail.com>
2023-10-27 19:14:42 -0300

    Criado dialog loja produto e mostrando a dialog ao selecionar um produto

--- app/src/main/java/com/example/projetocafeteria/fragment/loja/LojaProdutoFragment.java
+++ app/src/main/java/com/example/projetocafeteria/fragment/loja/LojaProdutoFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 
@@ -16,6 +17,7 @@ import android.widget.Toast;
 import com.example.projetocafeteria.R;
 import com.example.projetocafeteria.activity.loja.LojaFormProdutoActivity;
 import com.example.projetocafeteria.adapter.LojaProdutoAdapter;
+import com.example.projetocafeteria.databinding.DialogLojaProdutoBinding;
 import com.example.projetocafeteria.databinding.FragmentLojaProdutoBinding;
 import com.example.projetocafeteria.helper.FirebaseHelper;
 import com.example.projetocafeteria.model.Produto;
@@ -23,6 +25,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -35,6 +38,8 @@ public class LojaProdutoFragment extends Fragment implements LojaProdutoAdapter.
     private LojaProdutoAdapter lojaProdutoAdapter;
 
     private FragmentLojaProdutoBinding binding;
+
+    private AlertDialog dialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -103,8 +108,31 @@ public class LojaProdutoFragment extends Fragment implements LojaProdutoAdapter.
         });
     }
 
+    private void showDialog(Produto produto) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext(), R.style.CustomAlertDialog);
+
+        DialogLojaProdutoBinding dialogBinding = DialogLojaProdutoBinding
+                .inflate(LayoutInflater.from(requireContext()));
+
+        for (int i = 0; i < produto.getUrlsImagens().size(); i++) {
+            if (produto.getUrlsImagens().get(i).getIndex() == 0) {
+                Picasso.get().load(produto.getUrlsImagens().get(i).getCaminhoImagem())
+                        .into(dialogBinding.imagemProduto);
+            }
+        }
+
+        dialogBinding.txtNomeproduto.setText(produto.getTitulo());
+
+        dialogBinding.btnFechar.setOnClickListener(v -> dialog.dismiss());
+
+        builder.setView(dialogBinding.getRoot());
+
+        dialog = builder.create();
+        dialog.show();
+    }
+
     @Override
     public void onClick(Produto produto) {
-        Toast.makeText(requireContext(), produto.getTitulo(), Toast.LENGTH_SHORT).show();
+        showDialog(produto);
     }
 }
