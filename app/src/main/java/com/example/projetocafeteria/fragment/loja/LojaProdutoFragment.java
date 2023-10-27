505084b226d4597e51e2ccdf5b91ef6109304844
Weuller Oliveira <weullerwww@gmail.com>
2023-10-27 17:08:41 -0300

    Criado LojaProdutoAdapter e item produto adapter e listando produtos cadastrados da loja

--- app/src/main/java/com/example/projetocafeteria/fragment/loja/LojaProdutoFragment.java
+++ app/src/main/java/com/example/projetocafeteria/fragment/loja/LojaProdutoFragment.java
@@ -6,16 +6,33 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.projetocafeteria.R;
 import com.example.projetocafeteria.activity.loja.LojaFormProdutoActivity;
+import com.example.projetocafeteria.adapter.LojaProdutoAdapter;
 import com.example.projetocafeteria.databinding.FragmentLojaProdutoBinding;
+import com.example.projetocafeteria.helper.FirebaseHelper;
+import com.example.projetocafeteria.model.Produto;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
-public class LojaProdutoFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LojaProdutoFragment extends Fragment implements LojaProdutoAdapter.OnClickLister {
+
+    private List<Produto> produtoList = new ArrayList<>();
+
+    private LojaProdutoAdapter lojaProdutoAdapter;
 
     private FragmentLojaProdutoBinding binding;
 
@@ -32,6 +49,15 @@ public class LojaProdutoFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         configClicks();
+
+        configRv();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        recuperaProdutos();
     }
 
     private void configClicks() {
@@ -40,4 +66,45 @@ public class LojaProdutoFragment extends Fragment {
         });
     }
 
+    private void configRv() {
+        binding.rvProdutos.setLayoutManager(new GridLayoutManager(requireContext(), 2));
+        binding.rvProdutos.setHasFixedSize(true);
+        lojaProdutoAdapter = new LojaProdutoAdapter(produtoList, requireContext(), this);
+        binding.rvProdutos.setAdapter(lojaProdutoAdapter);
+    }
+
+    private void recuperaProdutos() {
+        DatabaseReference produtoRef = FirebaseHelper.getDatabaseReference()
+                .child("produtos");
+        produtoRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    produtoList.clear();
+                    for (DataSnapshot ds : snapshot.getChildren()) {
+                        Produto produto = ds.getValue(Produto.class);
+                        produtoList.add(produto);
+                    }
+                    binding.textInfo.setText("");
+                } else {
+                    binding.textInfo.setText("Nenhum produto cadastrado.");
+                }
+
+                binding.progressBar.setVisibility(View.GONE);
+                Collections.reverse(produtoList);
+                lojaProdutoAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(Produto produto) {
+        Toast.makeText(requireContext(), produto.getTitulo(), Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/com/example/projetocafeteria/model/Produto.java
+++ app/src/main/java/com/example/projetocafeteria/model/Produto.java
@@ -4,10 +4,11 @@ import com.example.projetocafeteria.helper.FirebaseHelper;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Exclude;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Produto {
+public class Produto implements Serializable {
 
     private String id; // id Firebase
     private int idLocal; //id Local
