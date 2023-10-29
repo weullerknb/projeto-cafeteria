ff12d6b4d16be593b8bcb2c317392bc5ad9a4b11
Weuller Oliveira <weullerwww@gmail.com>
2023-10-29 10:52:29 -0300

    LojaConfigActivity carregando o logo da galeria para a loja

--- app/src/main/java/com/example/projetocafeteria/fragment/loja/LojaConfigFragment.java
+++ app/src/main/java/com/example/projetocafeteria/fragment/loja/LojaConfigFragment.java
@@ -1,7 +1,10 @@
 package com.example.projetocafeteria.fragment.loja;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,13 +12,34 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.projetocafeteria.R;
+import com.example.projetocafeteria.activity.loja.LojaConfigActivity;
+import com.example.projetocafeteria.databinding.FragmentLojaConfigBinding;
 
 public class LojaConfigFragment extends Fragment {
+
+    private FragmentLojaConfigBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_loja_config, container, false);
+        binding = FragmentLojaConfigBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        configClicks();
+    }
+
+    private void configClicks() {
+        binding.btnConfigLoja.setOnClickListener(v -> {
+            startActivity(LojaConfigActivity.class);
+        });
+    }
+
+    private void startActivity(Class<?> clazz) {
+        startActivity(new Intent(requireContext(), clazz));
     }
 }
