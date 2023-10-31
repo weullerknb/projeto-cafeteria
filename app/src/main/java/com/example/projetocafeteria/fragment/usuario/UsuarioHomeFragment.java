f4d5fd99b865457f7ca5d5b5187137c194b665ce
Weuller Oliveira <weullerwww@gmail.com>
2023-10-31 13:58:31 -0300

    Refactor CategoriaAdapter para receber mais q um layout e recuperando as categorias do firebase e listando na tela home

--- app/src/main/java/com/example/projetocafeteria/fragment/usuario/UsuarioHomeFragment.java
+++ app/src/main/java/com/example/projetocafeteria/fragment/usuario/UsuarioHomeFragment.java
@@ -5,24 +5,91 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.projetocafeteria.R;
+import com.example.projetocafeteria.adapter.CategoriaAdapter;
+import com.example.projetocafeteria.databinding.FragmentUsuarioHomeBinding;
+import com.example.projetocafeteria.helper.FirebaseHelper;
+import com.example.projetocafeteria.model.Categoria;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
-public class UsuarioHomeFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class UsuarioHomeFragment extends Fragment implements CategoriaAdapter.OnClick {
+
+    private FragmentUsuarioHomeBinding binding;
+
+    private List<Categoria> categoriaList = new ArrayList<>();
+
+    private CategoriaAdapter categoriaAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_usuario_home, container, false);
+        binding = FragmentUsuarioHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        configRv();
+
+        recuperaCategorias();
+    }
+
+    private void configRv() {
+        binding.rvCategorias.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
+        binding.rvCategorias.setHasFixedSize(true);
+        categoriaAdapter = new CategoriaAdapter(R.layout.item_categoria_horizontal, true, categoriaList, this);
+        binding.rvCategorias.setAdapter(categoriaAdapter);
+    }
+
+    private void recuperaCategorias() {
+        DatabaseReference categoriaRef = FirebaseHelper.getDatabaseReference()
+                .child("categorias");
+        categoriaRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                categoriaList.clear();
+
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    Categoria categoria = ds.getValue(Categoria.class);
+                    categoriaList.add(categoria);
+                }
+
+                Collections.reverse(categoriaList);
+                categoriaAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onClickListener(Categoria categoria) {
+        Toast.makeText(requireContext(), categoria.getNome(), Toast.LENGTH_SHORT).show();
     }
 }
