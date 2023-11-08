9206c9ecc6391cef491f0f98db54b83bd6adf6ab
Weuller Oliveira <weullerwww@gmail.com>
2023-11-08 13:02:49 -0300

    Listando enderecos do usuario na tela selecao de endereco de entrega

--- app/src/main/java/com/example/projetocafeteria/activity/usuario/UsuarioFormEnderecoActivity.java
+++ app/src/main/java/com/example/projetocafeteria/activity/usuario/UsuarioFormEnderecoActivity.java
@@ -3,6 +3,7 @@ package com.example.projetocafeteria.activity.usuario;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -86,7 +87,12 @@ public class UsuarioFormEnderecoActivity extends AppCompatActivity {
                                 endereco.salvar();
                                 binding.progressBar.setVisibility(View.GONE);
 
-                                if (novoEndereco) finish();
+                                if (novoEndereco) {
+                                    Intent intent = new Intent();
+                                    intent.putExtra("enderecoCadastrado", endereco);
+                                    setResult(RESULT_OK, intent);
+                                    finish();
+                                }
 
                             } else {
                                 binding.edtMunicipio.requestFocus();
--- app/src/main/java/com/example/projetocafeteria/activity/usuario/UsuarioSelecionaEnderecoActivity.java
+++ app/src/main/java/com/example/projetocafeteria/activity/usuario/UsuarioSelecionaEnderecoActivity.java
@@ -1,15 +1,36 @@
 package com.example.projetocafeteria.activity.usuario;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-import com.example.projetocafeteria.R;
+import com.example.projetocafeteria.adapter.EnderecoSelecaoAdapter;
 import com.example.projetocafeteria.databinding.ActivityUsuarioSelecionaEnderecoBinding;
+import com.example.projetocafeteria.helper.FirebaseHelper;
+import com.example.projetocafeteria.model.Endereco;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
-public class UsuarioSelecionaEnderecoActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class UsuarioSelecionaEnderecoActivity extends AppCompatActivity implements EnderecoSelecaoAdapter.OnClickListener {
 
     private ActivityUsuarioSelecionaEnderecoBinding binding;
+
+    private EnderecoSelecaoAdapter enderecoSelecaoAdapter;
+
+    private final List<Endereco> enderecoList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +41,73 @@ public class UsuarioSelecionaEnderecoActivity extends AppCompatActivity {
         iniciaComponentes();
 
         configClicks();
+
+        configRv();
+
+        recuperaEndereco();
+    }
+
+    private void configRv() {
+        binding.rvEnderecos.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvEnderecos.setHasFixedSize(true);
+        enderecoSelecaoAdapter = new EnderecoSelecaoAdapter(enderecoList, this);
+        binding.rvEnderecos.setAdapter(enderecoSelecaoAdapter);
     }
 
     private void configClicks() {
         binding.include.include.ibVoltar.setOnClickListener(v -> finish());
+        binding.btnCadastrarEndereco.setOnClickListener(v -> {
+            resultLauncher.launch(new Intent(this, UsuarioFormEnderecoActivity.class));
+        });
+    }
+
+    private void recuperaEndereco() {
+        DatabaseReference enderecoRef = FirebaseHelper.getDatabaseReference()
+                .child("enderecos")
+                .child(FirebaseHelper.getIdFirebase());
+        enderecoRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    enderecoList.clear();
+                    for (DataSnapshot ds : snapshot.getChildren()) {
+                        Endereco endereco = ds.getValue(Endereco.class);
+                        enderecoList.add(endereco);
+                    }
+                    binding.textInfo.setText("");
+                } else {
+                    binding.textInfo.setText("Nenhuma endereço cadastrado.");
+                }
+                binding.progressBar.setVisibility(View.GONE);
+                Collections.reverse(enderecoList);
+                enderecoSelecaoAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void iniciaComponentes() {
         binding.include.textTitulo.setText("Endereço de Entrega");
+    }
+
+    private final ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    Endereco endereco = (Endereco) result.getData().getSerializableExtra("enderecoCadastrado");
+                    enderecoList.add(endereco);
+                    binding.textInfo.setText("");
+                    enderecoSelecaoAdapter.notifyItemInserted(enderecoList.size());
+                }
+            }
+    );
+
+    @Override
+    public void onClick(Endereco endereco) {
+        Toast.makeText(this, endereco.getNomeEndereco(), Toast.LENGTH_SHORT).show();
     }
 }
