4728b7dac1ec45ca6a87d58812a1a7c801159f6c
Weuller Oliveira <weullerwww@gmail.com>
2023-11-14 08:13:36 -0300

    Criado layout item_produto_detalhe_pedido e activity_detalhes_pedido

---
+++ app/src/main/java/com/example/projetocafeteria/activity/app/DetalhesPedidoActivity.java
@@ -0,0 +1,16 @@
+package com.example.projetocafeteria.activity.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projetocafeteria.R;
+
+public class DetalhesPedidoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_pedido);
+    }
+}
